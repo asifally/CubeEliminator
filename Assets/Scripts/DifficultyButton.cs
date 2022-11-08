f49afc481a7afaeba35fada89c3becf85b068ef9
Asif Ally <asifally004@gmail.com>
2022-11-08 16:37:24 -0600

    Added data persistence across scenes and session and a new menu

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -8,17 +8,15 @@ public class DifficultyButton : MonoBehaviour
     public int difficulty;
 
     private Button button;
-    private GameManager gameManagerScript;
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
-        gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     void SetDifficulty()
     {
-        gameManagerScript.StartGame(difficulty);
+        DataManager.Instance.Difficulty = difficulty;
     }
 }
